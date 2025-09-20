1859dc07b6ef2cb81ec8dd7f82beb6611e2bbbea
sooyong <jrsooyong@gmail.com>
2025-09-20 20:57:42 +0900

    Feat: Add create department form

--- core/static/core/js/org_chart.js
+++ core/static/core/js/org_chart.js
@@ -1,11 +1,25 @@
 function handleMenuClick(action) {
-    alert('Clicked: ' + action);
+    // alert('Clicked: ' + action);
     // 여기에 선택된 메뉴 항목에 대한 동작을 구현합니다.
     // 예시: 조직도 영역의 내용을 변경
     const contentArea = document.querySelector('.content');
     switch (action) {
         case 'create_department':
-            contentArea.innerHTML = '<h2>Create Department</h2><p>Creating a new department...</p>';
+            contentArea.innerHTML = `
+                <h2>Create Department</h2>
+                <form>
+                    <label for="department_name">Name:</label><br>
+                    <input type="text" id="department_name" name="department_name"><br>
+                    <label for="creation_date">Creation Date:</label><br>
+                    <input type="date" id="creation_date" name="creation_date"><br>
+                    <label for="parent_department">Parent Department:</label><br>
+                    <select id="parent_department" name="parent_department">
+                        <option value="">-- Select --</option>
+                        <!-- 여기에 상위 조직 목록을 동적으로 채워야 합니다. -->
+                    </select><br><br>
+                    <button type="button" onclick="createDepartment()">Create</button>
+                </form>
+            `;
             break;
         case 'move_department':
             contentArea.innerHTML = '<h2>Move Department</h2><p>Moving a department...</p>';
